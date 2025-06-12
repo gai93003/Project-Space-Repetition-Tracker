95798457291b45e38fb704a17dd6bb118e644822
Gabriel Deng <deng93003@gmail.com>
2025-06-12 18:23:45 +0100

    Made some final touches on the tests and js functions.

--- common.test.js
+++ common.test.js
@@ -7,7 +7,7 @@ import { getUserIDs, dateRepetion, dateFormat } from "./common.mjs";
 // });
 
 test ("Date should be in the right format", () => {
-  const dateObj = new Date();
+  const dateObj = new Date(2025, 5, 9);
   const expectedDate = `9th June 2025`;
 
   expect(dateFormat(dateObj)).toBe(expectedDate);
@@ -24,4 +24,17 @@ test ("Revision dates should be repeated in intervals of 1 week, 1 month etc", (
   expect(dateFormat(revisionDates[0].date)).toBe("1st April 2015");
   expect(dateFormat(revisionDates[1].date)).toBe("25th April 2015");
   expect(dateFormat(revisionDates[4].date)).toBe("25th March 2016");
+});
+
+describe("Tests for getUserIds function.", () => {
+  let users = getUserIDs();
+
+  test("There should be 5 users from getUserId function", () => {
+    expect(users.length).toBe(5);
+  });
+
+  test("getUserIds function should always return an array which is not empty.", () => {
+    expect(Array.isArray(users)).toBe(true);
+    expect(users.length === 0).toBe(false);
+  }); 
 });
